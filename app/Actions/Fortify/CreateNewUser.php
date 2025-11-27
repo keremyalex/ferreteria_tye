55905767f1f6bb1e16f012ddbff81e69cb29c9d4
alex keremy <keremy.alex@gmail.com>
2025-11-27 05:21:07 -0400

    added rol cliente new users

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -26,10 +26,15 @@ class CreateNewUser implements CreatesNewUsers
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
-        return User::create([
+        $user = User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
+
+        // Asignar automáticamente el rol de cliente a usuarios registrados
+        $user->assignRole('cliente');
+
+        return $user;
     }
 }
